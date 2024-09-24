edfd6f6daf226e8d5cb0fd3ce803bc03d9eccd55
Louis C <louischoiniere@outlook.com>
2024-09-24 12:58:32 -0400

    wip

--- CodebustersPAS-Server/API/Controllers/AuthentificationController.cs
+++ CodebustersPAS-Server/API/Controllers/AuthentificationController.cs
@@ -1,9 +1,74 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Claims;
+using Infrastructure;
+using Infrastructure.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthentificationController : ControllerBase {
+namespace API.Controllers;
 
+[Route("api/[controller]")]
+[ApiController]
+public class AuthentificationController : ControllerBase {
+
+    private readonly ILogger<StudentController> _logger;
+    private readonly PeerAssessmentSystemDbContext _dbContext;
+
+    public AuthentificationController(ILogger<StudentController> logger, PeerAssessmentSystemDbContext dbContext) {
+        _logger = logger;
+        _dbContext = dbContext;
     }
+
+    [HttpPost, ActionName("LogIn")]
+    public async Task<ActionResult> LogIn(LogInDTO logInDTO) {
+
+        User user = await _dbContext.Users.FirstAsync(User => User.username == logInDTO.username);
+
+        // Check password
+        // Todo: hash password and not to store it as plain text
+        if(!logInDTO.password.Equals(user.Password)){
+            // WRONG PASSOWRD
+            return Unauthorized();
+        }
+
+        var claims = new List<Claim> {
+            new Claim(ClaimTypes.Name, user.FirstName)
+        };
+
+        var identity = new ClaimsIdentity(claims);
+        var principal = new ClaimsPrincipal(identity);
+
+        await HttpContext.SignInAsync(principal);
+
+        throw new NotImplementedException();
+    }
+
+    [HttpPost, ActionName("SignUp")]
+    public async Task<ActionResult> SignUp(SignUpDTO signUpDTO) {
+
+
+        throw new NotImplementedException();
+    }
+
+    [HttpPost, ActionName("LogOut")]
+    public async Task<ActionResult> LogOut() {
+        await HttpContext.SignOutAsync();
+
+        return Ok();
+    }
+
+    public record LogInDTO(
+        int userType,
+        string username,
+        string password
+    );
+
+    public record SignUpDTO(
+        int userType,
+        string firstName,
+        string lastName,
+        int studentId,
+        string username,
+        string password
+    );
 }
--- CodebustersPAS-Server/API/Controllers/StudentController.cs
+++ CodebustersPAS-Server/API/Controllers/StudentController.cs
@@ -17,61 +17,61 @@ namespace API.Controllers {
             _dbContext = dbContext;
         }
 
-        [HttpGet("{id}"), ActionName("GetStudent")]
-        public async Task<ActionResult<Student>> Get(Guid id) {
-            var student = await _dbContext.Users
-                .Where(User => User.type == UserType.Student)
-                .FirstOrDefaultAsync(User => User.Id.Equals(id));
+        // [HttpGet("{id}"), ActionName("GetStudent")]
+        // public async Task<ActionResult<Student>> Get(Guid id) {
+        //     var student = await _dbContext.Users
+        //         .Where(User => User.type == UserType.Student)
+        //         .FirstOrDefaultAsync(User => User.Id.Equals(id));
 
-            if (student is null) {
-                return NotFound();
-            }
+        //     if (student is null) {
+        //         return NotFound();
+        //     }
 
-            return new Student(student.Id, student.FirstName, student.LastName);
-        }
+        //     return new Student(student.Id, student.FirstName, student.LastName);
+        // }
 
-        [HttpGet, ActionName("GetAllStudent")]
-        public async Task<ActionResult<IEnumerable<Student>>> GetAll() {
-            var students = await _dbContext.Users
-                .Where(User => User.type == UserType.Student)
-                .ToListAsync();
+        // [HttpGet, ActionName("GetAllStudent")]
+        // public async Task<ActionResult<IEnumerable<Student>>> GetAll() {
+        //     var students = await _dbContext.Users
+        //         .Where(User => User.type == UserType.Student)
+        //         .ToListAsync();
 
 
-            return students
-                .Select(student => new Student(student.Id, student.FirstName, student.LastName))
-                .ToList();
-        }
+        //     return students
+        //         .Select(student => new Student(student.Id, student.FirstName, student.LastName))
+        //         .ToList();
+        // }
 
-        [HttpPost, ActionName("CreateStudent")]
-        public async Task<ActionResult<Student>> Post(NewStudent student) {
-            var newStudent = _dbContext.Users.Add(
-               new User {
-                   Id = new Guid(),
-                   FirstName = student.FirstName,
-                   LastName = student.LastName,
-                   type = UserType.Student,
-               });
-            await _dbContext.SaveChangesAsync();
+        // [HttpPost, ActionName("CreateStudent")]
+        // public async Task<ActionResult<Student>> Post(NewStudent student) {
+        //     var newStudent = _dbContext.Users.Add(
+        //        new User {
+        //            Id = new Guid(),
+        //            FirstName = student.FirstName,
+        //            LastName = student.LastName,
+        //            type = UserType.Student,
+        //        });
+        //     await _dbContext.SaveChangesAsync();
 
-            return new Student(newStudent.Entity.Id, newStudent.Entity.FirstName, newStudent.Entity.LastName);
-        }
+        //     return new Student(newStudent.Entity.Id, newStudent.Entity.FirstName, newStudent.Entity.LastName);
+        // }
 
-        [HttpDelete, ActionName("DeleteStudent")]
-        public async Task<ActionResult<Student>> Delete(Guid id) {
+        // [HttpDelete, ActionName("DeleteStudent")]
+        // public async Task<ActionResult<Student>> Delete(Guid id) {
 
-            var student = await _dbContext.Users.FirstOrDefaultAsync(student => student.Id == id);
+        //     var student = await _dbContext.Users.FirstOrDefaultAsync(student => student.Id == id);
 
-            if (student is null) {
-                return NotFound();
-            }
+        //     if (student is null) {
+        //         return NotFound();
+        //     }
 
-            var studentDTO = new Student(student.Id, student.FirstName, student.LastName);
+        //     var studentDTO = new Student(student.Id, student.FirstName, student.LastName);
 
-            _dbContext.Users.Remove(student);
-            await _dbContext.SaveChangesAsync();
+        //     _dbContext.Users.Remove(student);
+        //     await _dbContext.SaveChangesAsync();
 
-            return studentDTO;
-        }
+        //     return studentDTO;
+        // }
 
         public record Student(Guid id, string FirstName, string LastName);
         public record NewStudent(string FirstName, string LastName);
