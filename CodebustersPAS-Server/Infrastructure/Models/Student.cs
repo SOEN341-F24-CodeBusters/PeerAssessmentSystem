4282930ce23ca0d5718add79cb9f1c2a34d7773f
Louis Choiniere <64211882+LouisChoiniere@users.noreply.github.com>
2024-10-16 10:27:15 -0400

    fix databse and add migration (#57)

--- CodebustersPAS-Server/Infrastructure/Models/Student.cs
+++ CodebustersPAS-Server/Infrastructure/Models/Student.cs
@@ -1,5 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Infrastructure.Models {
-    public class Student: User {
+    public class Student {
+
+        [Key]
+        public Guid Id { get; set; }
+
         public int StudentID { get; set; }
+
+
+        [NotMapped]
+        public User? user { get; set; }
+        public List<Group> Groups { get; set; }
+        public List<Team> Teams { get; set; }
     }
 }
