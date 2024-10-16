3951e35d25db8ef34d8566d265cbf49ca3e73284
Theodor Salcianu <theosalcianu10@gmail.com>
2024-10-15 22:08:13 -0400

    Added the Logout button & functionality for Student

--- CodebustersPAS-ClientApp/src/pages/Students/PeerAssessment.jsx
+++ CodebustersPAS-ClientApp/src/pages/Students/PeerAssessment.jsx
@@ -1,19 +1,11 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./CourseListingStudentsPage.css";
 import CourseItem from "./CourseItem";
 
 function PeerAssessment() {
-  // const courseData = [
-  //   {
-  //     name: "SOEN341",
-  //     description: "This is description for the course",
-  //     startDate: "03/09/2024",
-  //     endDate: "02/12/2024",
-  //     status: "Ready",
-  //   },
-  // ];
-
   const [courseData, setCourseData] = React.useState([]);
+  const navigate = useNavigate();
 
   React.useEffect(() => {
     updateCourseData();
@@ -21,12 +13,11 @@ function PeerAssessment() {
 
   async function updateCourseData() {
     let temp_data = await getCourseData();
-
     setCourseData(temp_data);
   }
 
   async function getCourseData() {
-    //here needs to be actual request
+    // Here needs to be the actual request.
     let temp_data = [
       {
         name: "SOEN341",
@@ -36,15 +27,45 @@ function PeerAssessment() {
         status: "Ready",
       },
     ];
-
     return temp_data;
   }
+
+  const handleLogout = async () => {
+    const apiUrl = 'https://localhost:7010/api/Authentification/LogOut';
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        const responseData = await response.json(); 
+        console.log('Logout successful:', responseData)
+
+        navigate('/');
+      } else {
+        console.error('Logout failed with status:', response.status);
+      }
+    } catch (error) {
+      console.error('An error occurred during logout:', error);
+    }
+  };
 
   return (
     <div className="container">
       <header className="header">
         <h1 className="title">Peer Assessment</h1>
         <nav className="studentTab">Student's Tab</nav>
+        <button
+          onClick={handleLogout} className="logout-button"
+          onMouseEnter={(e) => (e.target.classList.add('hover'))}
+          onMouseLeave={(e) => (e.target.classList.remove('hover'))}
+        >
+          Logout
+        </button>
       </header>
 
       <section className="courseContainer">
@@ -67,3 +88,4 @@ function PeerAssessment() {
 }
 
 export default PeerAssessment;
+
