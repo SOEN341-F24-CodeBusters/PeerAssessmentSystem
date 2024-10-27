5680b964d77d2b5e963c5a051bd1f6f9db74ce81
Paciflow <pacifique06@gmail.com>
2024-10-27 13:15:23 -0400

    csv_import by Pacifique

---
+++ CodebustersPAS-ClientApp/src/pages/Teacher/CreateTeam/CSV_Import/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+
+import React from 'react';
+import CSVUpload from './Components/CSVUploads';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Team Management</h1>
+      <CSVUpload />
+    </div>
+  );
+};
+
+export default App;
