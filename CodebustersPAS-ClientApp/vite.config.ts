0e9cc27e7b781f200bf78fc08c3704ea9d3058f7
tinaw1412 <jingnan1412@gmail.com>
2024-10-31 17:33:29 -0400

    fix2

--- CodebustersPAS-ClientApp/vite.config.ts
+++ CodebustersPAS-ClientApp/vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['test/setupTests.ts'],
-  },
+  plugins: [react(), basicSsl()],
 })
+
