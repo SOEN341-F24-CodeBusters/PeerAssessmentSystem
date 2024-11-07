eb95609c13335080282b63d1093bba903ed929f5
vRizon <nikandrovavaleriia@gmail.com>
2024-11-07 18:14:34 -0500

    Fixed height & Added all dimensions

--- CodebustersPAS-ClientApp/src/pages/CourseEvaluation/GroupEvaluation.jsx
+++ CodebustersPAS-ClientApp/src/pages/CourseEvaluation/GroupEvaluation.jsx
@@ -23,8 +23,29 @@ const GroupEvaluation = () => {
       </p>
 
       <AssessmentDimension
-        title="COOPERATION"
+        title="Cooperation"
         description="Actively participating in meetings; Communicating within the group; Cooperating within the group; Assisting team-mates when needed; Volunteering for tasks."
+        members={["John Doe", "John Doe", "John Doe"]}
+      />
+      <AssessmentDimension
+        title="Conceptual Contribution"
+        description="Researching and gathering information; Quality of
+ individual contribution; Suggesting ideas; Tying ideas together; Identifying difficulties;
+ Identifying effective approaches."
+        members={["John Doe", "John Doe", "John Doe"]}
+      />
+      <AssessmentDimension
+        title="Practical Contribution"
+        description="Writing of the report(s); Reviewing others’ report(s) or
+ section(s); Providing constructive feedback on the report(s) or the presentation;
+ Contributing to the organization of the work; Contributing to the preparation of
+ presentation(s) (if appropriate)."
+        members={["John Doe", "John Doe", "John Doe"]}
+      />
+      <AssessmentDimension
+        title="Work Ethic"
+        description="Displaying a positive attitude; Respecting team-mates; Respecting
+ commitments; Respecting deadlines; Respecting team-mates’ ideas."
         members={["John Doe", "John Doe", "John Doe"]}
       />
 
