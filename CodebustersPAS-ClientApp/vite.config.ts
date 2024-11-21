706b95960fa4f1efab4e61fbbfbcd8abf3a643a2
louischoiniere <louischoiniere@outlook.com>
2024-11-20 23:03:09 -0500

    help.

--- CodebustersPAS-ClientApp/vite.config.ts
+++ CodebustersPAS-ClientApp/vite.config.ts
@@ -1,15 +1,23 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['test/setupTests.ts'],
   },
+  server: {
+    watch: {
+      usePolling: true,
+    }
+  },
+  preview: {
+    host: 'localhost',
+    port: 8080
+  }
 })
 
